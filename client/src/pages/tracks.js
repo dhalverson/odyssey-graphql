d08e283f8a2291717bfecef667ebfbf6dd5ef6d7
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2022-08-09 16:27:14 -0600

    Updates tracks to use queryResult

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Layout } from '../components';
 import { gql, useQuery } from '@apollo/client'; 
 import TrackCard from '../containers/track-card';
-
+import QueryResult from '../components/query-result';
 
 const TRACKS = gql`
   query GetTracks {
@@ -27,15 +27,15 @@ const TRACKS = gql`
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
   
-  if (loading) return 'Loading...';
-
-  if (error) return `ERROR! ${error.message}`;
-
-  return <Layout grid>
-      {data?.tracksForHome?.map(track => (
-        <TrackCard key={track.id} track={track} />
-      ))}
-    </Layout>;
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+          {data?.tracksForHome?.map((track) => (
+            <TrackCard key={track.id} track={track} />
+          ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
